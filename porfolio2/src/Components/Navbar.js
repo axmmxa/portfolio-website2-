b73818c3873b1973c226e7c96c558b2cce91f0a9
Alexander Much <46798654+axmmxa@users.noreply.github.com>
2023-08-06 01:59:31 +0200

    Navbar Elements added

---
+++ porfolio2/src/Components/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './styles/Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className='Navbar'>
+      <h3>alex much</h3>
+      <div className='Nav-list'>
+        <ul>
+           <li>ABOUT ME</li>
+           <li>SKILLS</li>
+           <li>EXPIERENCE</li>
+           <li>CONTACT</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
