b8bdfd49b35d31cb42d997a00636c11a91031a9d
Alexander Much <46798654+axmmxa@users.noreply.github.com>
2023-08-06 02:19:32 +0200

    text added

---
+++ porfolio2/src/Components/Main.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './styles/Main.css'
+const Main = () => {
+  return (
+    <div className='main'>
+      <h1>Hello- -</h1>
+      <h1>I'm Alexander Much</h1>
+    </div>
+  )
+}
+
+export default Main
