946acdda4a821e7e679b2d492795d1bb6702fe3e
Alexander Much <46798654+axmmxa@users.noreply.github.com>
2023-08-15 23:46:33 +0200

    main component added

--- porfolio2/src/Components/Main.js
+++ porfolio2/src/Components/Main.js
@@ -4,8 +4,21 @@ import './styles/Main.css'
 const Main = () => {
   return (
     <div className='main'>
-      <h1>Hello- -</h1>
-      <h1>I'm Alexander Much</h1>
+
+      <div className='hello'>
+        <h1>Hello</h1>
+         <p>This IS MY PORTFOLIO</p> 
+      </div>
+
+      <div className='myName'>
+        <h1>I'M ALEX</h1>
+        <div className='links'>
+          <a>LInKED IN</a>
+          <a>XING</a>
+          <a>DOWNLOAD CV</a>
+        </div>
+      </div>
+
     </div>
   )
 }
