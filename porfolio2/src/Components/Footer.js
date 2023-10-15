ea4c7cca9d017ca110a9e36d51f8837182b282ab
Alexander Much <46798654+axmmxa@users.noreply.github.com>
2023-10-15 21:56:10 +0200

    footer responsive added

--- porfolio2/src/Components/Footer.js
+++ porfolio2/src/Components/Footer.js
@@ -4,14 +4,30 @@ import logosmall from '../images/logo-small.svg'
 
 const Footer = () => {
   return (
+
+    <>
+    
     <div className='footer container content-left'>
-      <div className='impressum'>
-        <a>PRIVACY</a>
-        <a>IMPRESSUM</a>
-      </div>
-      <img src={logosmall} className='logo-size'></img>
-      <a>COPYRIGHT ALEXANDER MUCH 2023</a>
+    <div className='impressum'>
+      <a>PRIVACY</a>
+      <a>IMPRESSUM</a>
     </div>
+    <img src={logosmall} className='logo-size'></img>
+    <a>COPYRIGHT ALEXANDER MUCH 2023</a>
+    </div>
+
+
+    <div className='footer-mobile container content-left'>
+      <img src={logosmall} className='logo-size'></img>
+
+        <div className='impressum'>
+          <a>PRIVACY</a>
+          <a>IMPRESSUM</a>
+      </div>
+    </div>
+
+
+    </>
   )
 }
 
