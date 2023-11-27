a11d9fdbbb6a571523e82a5463a1416bef1c684c
Alexander Much <46798654+axmmxa@users.noreply.github.com>
2023-11-27 13:30:51 +0100

    form states updated

--- porfolio2/src/Components/Contact.js
+++ porfolio2/src/Components/Contact.js
@@ -1,10 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Contactlogo from '../images/Contact.svg'
 import  './styles/Contact.css'
 
 import arrow from '../images/Arrow 1.svg'
 
 const Contact = () => {
+
+  const [name,setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [text, setText] = useState('')
+
+  const handleNameChange = (e) => {
+    setName(e.target.value)
+  }
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value)
+  }
+   
+  const handleTextChange = (e) => {
+    setText(e.target.value)
+  }
+  
+  
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(name, email, text)
+  }
+  
+
   return (
    <>
         <div className='contact-logo spacer-tall  content-left'>
@@ -14,11 +38,11 @@ const Contact = () => {
         <div id='contact' className='contact content-left '>
             <h1>Let's talk</h1>
 
-            <form className='contact-form'>
-                <input type='text' name='Name' placeholder='Name'></input>
-                <input type='email'name='email' placeholder='Email'></input>
-                <textarea placeholder='Message' cols={5} rows={6}></textarea>
-                <button>SEND MSG <img src={arrow}></img></button>
+            <form className='contact-form' onSubmit={handleSubmit}>
+                <input type='text' name='Name' value={name} onChange={handleNameChange} id='' placeholder='Name'></input>
+                <input type='email'name='email' value={email} onChange={handleEmailChange} id=''  placeholder='Email'></input>
+                <textarea type='text' name='' value={text}  placeholder='Message' onChange={handleTextChange} cols={5} rows={6}></textarea>
+                <button type='submit' >SEND MSG <img src={arrow}></img></button>
             </form>
 
         
